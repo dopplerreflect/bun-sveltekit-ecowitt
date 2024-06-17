7b6c674709d73cd0888d267f61cd414c71c32477
David Rose <doppler@gmail.com>
2024-06-17 20:56:38 +0000

    fix copy-database script

--- scripts/copy-database.sh
+++ scripts/copy-database.sh
@@ -3,9 +3,10 @@ HOST=$(hostname)
 
 echo $HOST
 
-if [ $HOST="thinkpad" ]; then
+if [[ $HOST = "thinkpad" ]]; then
   echo "thinkpad"
   scp pi:/var/tmp/bun-sveltekit-ecowitt.db.sqlite3 /var/tmp/bun-sveltekit-ecowitt.db.sqlite3
 else
   scp thinkpad:/var/tmp/bun-sveltekit-ecowitt.db.sqlite3 /var/tmp/bun-sveltekit-ecowitt.db.sqlite3
 fi
+
