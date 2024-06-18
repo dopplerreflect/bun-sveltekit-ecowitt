181663124d21f06b254a0a9dcdf46ba1d4dce7ba
David Rose <doppler@gmail.com>
2024-06-18 10:24:33 -0500

    style windchart placeholder

--- src/lib/components/WindChart.svelte
+++ src/lib/components/WindChart.svelte
@@ -1,1 +1,16 @@
-<h1>Wind Chart</h1>
+<main>
+  <header>Wind Chart</header>
+
+  <p>Placeholder for a graph.</p>
+</main>
+
+<style>
+  main {
+    height: 100%;
+    background-color: oklch(25% 25% var(--hue));
+  }
+  header {
+    font-size: 2em;
+    border-bottom: 1px solid white;
+  }
+</style>
